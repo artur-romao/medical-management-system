f84f8ed8cd8b1c71a540bcb94ecfb5e7a8a6f00a
dxogo <diogophc@ua.pt>
2021-12-15 22:55:46 +0000

    nesga

--- Project/medmanagesystem/src/main/java/com/mms/medmanagesystem/models/Consulta.java
+++ Project/medmanagesystem/src/main/java/com/mms/medmanagesystem/models/Consulta.java
@@ -1,8 +1,6 @@
 package com.mms.medmanagesystem.models;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
