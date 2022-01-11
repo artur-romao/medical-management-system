4bec5ccc0ae3161a3945bee7fe44298871c3f12b
artur-romao <arturomao@gmail.com>
2022-01-11 18:53:47 +0000

    login controller ended, waiting for db to start sessions

--- Project/medmanagesystem/src/main/java/com/mms/medmanagesystem/controller/LoginController.java
+++ Project/medmanagesystem/src/main/java/com/mms/medmanagesystem/controller/LoginController.java
@@ -1,15 +1,36 @@
 package com.mms.medmanagesystem.controller;
 
+import com.mms.medmanagesystem.model.LoginCredentials;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
 
-  @GetMapping("/login")
+  @ModelAttribute("loginCredentials")
+  public LoginCredentials loginCredentialsAttribute() {
+    return new LoginCredentials();
+  }
+
+  @GetMapping("/")
   public String login(Model model) {
     return "login";
   }
 
+  @PostMapping("/")
+  public String loginChecking(@ModelAttribute LoginCredentials loginCredentials, Model model) {
+    String medicoId = loginCredentials.getMedicoid();
+    String password = loginCredentials.getPassword();
+    if (password.equals("password")) {
+      return "index";
+    }
+    else {
+      model.addAttribute("error", "Dados de sessão inválidos!");
+      return "login";
+    }
+  }
 }
---
+++ Project/medmanagesystem/src/main/java/com/mms/medmanagesystem/model/LoginCredentials.java
@@ -0,0 +1,24 @@
+package com.mms.medmanagesystem.model;
+
+public class LoginCredentials {
+
+    private String medicoid;
+    private String password;
+  
+    public String getMedicoid() {
+      return medicoid;
+    }
+  
+    public void setMedicoid(String id) {
+      this.medicoid = id;
+    }
+  
+    public String getPassword() {
+      return password;
+    }
+  
+    public void setPassword(String password) {
+      this.password = password;
+    }
+  
+  }
