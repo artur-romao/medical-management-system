a3450187b89008c61760c3993c83ab3545a89344
marianarosa01 <marianarosa@ua.pt>
2022-01-12 16:22:06 +0000

    working

--- Project/medmanagesystem/src/main/java/com/mms/medmanagesystem/controller/LoginController.java
+++ Project/medmanagesystem/src/main/java/com/mms/medmanagesystem/controller/LoginController.java
@@ -1,4 +1,4 @@
-package com.mms.medmanagesystem.controller;
+/* package com.mms.medmanagesystem.controller;
 
 import com.mms.medmanagesystem.model.LoginCredentials;
 
@@ -7,21 +7,27 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/api/")
 public class LoginController {
-
+/* 
   @ModelAttribute("loginCredentials")
   public LoginCredentials loginCredentialsAttribute() {
     return new LoginCredentials();
   }
 
-  @GetMapping("/")
+  @GetMapping(value="/login") 
+  @ResponseBody
   public String login(Model model) {
     return "login";
   }
 
-  @PostMapping("/")
+  @PostMapping("/{user}")
   public String loginChecking(@ModelAttribute LoginCredentials loginCredentials, Model model) {
     String medicoId = loginCredentials.getMedicoid();
     String password = loginCredentials.getPassword();
@@ -33,4 +39,4 @@ public class LoginController {
       return "login";
     }
   }
-}
+} */
