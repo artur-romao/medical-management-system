f58d99c2aba09f18a9681ac59acfffe02fd0da73
dxogo <diogophc@ua.pt>
2022-01-12 23:59:11 +0000

    a

--- Project/medmanagesystem/src/main/java/com/mms/medmanagesystem/MedmanagesystemApplication.java
+++ Project/medmanagesystem/src/main/java/com/mms/medmanagesystem/MedmanagesystemApplication.java
@@ -64,8 +64,8 @@ public class MedmanagesystemApplication implements CommandLineRunner {
 
 		}
 		
-		if (areaService.getAreas().isEmpty()) {
-			
+		if (pessoaService.getPessoas().isEmpty()) {
+
 			medicoService.saveMedico(new Medico("7576664675766646", (new Pessoa(75766646, "Maia Mitchell", "maiamitchell@hotmail.com", 966265836, "153-5716 Odio, Rd", "21/10/1984")), areaRepository.getById(14)));
 			medicoService.saveMedico(new Medico("7385680573856805", (new Pessoa(73856805, "Leslie Landry", "leslielandry801@hotmail.com", 933236833, "Ap #744-1568 Luctus Street","10/12/1959" )), areaRepository.getById(2)));
 			medicoService.saveMedico(new Medico("6041232860412328", (new Pessoa(60412328, "Charles Morris", "charlesmorris@google.com", 914721284, "963-7884 Ipsum St.","20/03/1960" )), areaRepository.getById(5)));
