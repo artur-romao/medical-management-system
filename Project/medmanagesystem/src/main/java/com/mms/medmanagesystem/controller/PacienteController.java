4d4e8bfe2c5d6d2c5faec8ca1a3026c30f486c39
dxogo <diogophc@ua.pt>
2022-01-12 23:39:23 +0000

    nng mexe spring quieto pah nem pensem

--- Project/medmanagesystem/src/main/java/com/mms/medmanagesystem/controller/PacienteController.java
+++ Project/medmanagesystem/src/main/java/com/mms/medmanagesystem/controller/PacienteController.java
@@ -1,13 +1,5 @@
 package com.mms.medmanagesystem.controller;
 
-<<<<<<< HEAD
-import com.mms.medmanagesystem.service.PacienteService;
-import com.mms.medmanagesystem.model.Paciente;
-
-import java.util.List;
-import org.springframework.stereotype.Controller;
-=======
->>>>>>> 3735c4dad7a8ff2a5a600eb44650e1ed6ef5523e
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
