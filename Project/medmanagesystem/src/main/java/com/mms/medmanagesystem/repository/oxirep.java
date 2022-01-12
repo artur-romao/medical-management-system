3f4fc6059afbc9825fbf992bb7e71e5630e6af6a
PauloGSP <xuldir@pop-os.localdomain>
2022-01-12 01:00:28 +0000

    fix

--- Project/medmanagesystem/src/main/java/com/mms/medmanagesystem/repository/oxirep.java
+++ Project/medmanagesystem/src/main/java/com/mms/medmanagesystem/repository/oxirep.java
@@ -6,6 +6,7 @@ import com.mms.medmanagesystem.model.Internamento;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
+
 public interface oxirep extends JpaRepository<Internamento,Integer> {
 }
 */
