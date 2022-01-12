6330074c53f38e3aff1ff31124a7eb5177a9fa7d
marianarosa01 <marianarosa@ua.pt>
2022-01-12 22:05:57 +0000

    html patients

--- Project/medmanagesystem/src/main/java/com/mms/medmanagesystem/controller/PacienteController.java
+++ Project/medmanagesystem/src/main/java/com/mms/medmanagesystem/controller/PacienteController.java
@@ -1,16 +1,37 @@
 package com.mms.medmanagesystem.controller;
 
+import com.mms.medmanagesystem.service.PacienteService;
+import com.mms.medmanagesystem.model.Paciente;
+
+import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+
 
 @RestController
 public class PacienteController {
 
-  @GetMapping("/pacientes")
-  public String paciente(Model model) {
-    return "tables/pacientes";
+  @AutoWired private PacienteService service;
+  
+  @RequestMapping("/pacientes")
+  public String showListPacientes(Model model) {
+    
+    List<Paciente> listaPacientes = service.getPacientes();
+    model.addAttribute("listaPacientes", listaPacientes);
+    return "login";
+    
   }
+  
+
+/* 
+  @RequestMapping(value="name", method= {RequestMethod.GET})
+  public String name(@RequestParam String name, Model model){
+      String name =   
+  } */
+  
 
 }
