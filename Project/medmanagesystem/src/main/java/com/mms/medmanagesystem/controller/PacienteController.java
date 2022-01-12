3735c4dad7a8ff2a5a600eb44650e1ed6ef5523e
artur-romao <arturomao@gmail.com>
2022-01-12 20:21:06 +0000

    restcontrollers up sessions mid term

--- Project/medmanagesystem/src/main/java/com/mms/medmanagesystem/controller/PacienteController.java
+++ Project/medmanagesystem/src/main/java/com/mms/medmanagesystem/controller/PacienteController.java
@@ -1,16 +1,18 @@
 package com.mms.medmanagesystem.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class PacienteController {
 
   @GetMapping("/pacientes")
-  public String paciente(Model model) {
-    return "tables/pacientes";
+  public ModelAndView paciente(Model model) {
+    ModelAndView modelAndView = new ModelAndView();
+    modelAndView.setViewName("tables/pacientes");
+    return modelAndView;
   }
 
 }
--- Project/medmanagesystem/src/main/java/com/mms/medmanagesystem/controller/PerfilController.java
+++ Project/medmanagesystem/src/main/java/com/mms/medmanagesystem/controller/PerfilController.java
@@ -1,16 +1,18 @@
 package com.mms.medmanagesystem.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class PerfilController {
 
   @GetMapping("/perfil")
-  public String perfil(Model model) {
-    return "perfil";
+  public ModelAndView perfil(Model model) {
+    ModelAndView modelAndView = new ModelAndView();
+    modelAndView.setViewName("perfil");
+    return modelAndView;
   }
 
 }
