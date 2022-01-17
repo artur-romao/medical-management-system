f7ff9f6cc5b221fcf517176c4f957750a253898f
marianarosa01 <marianarosa@ua.pt>
2022-01-17 01:50:49 +0000

    changes

--- Project/medmanagesystem/src/main/java/com/mms/medmanagesystem/controller/PacienteController.java
+++ Project/medmanagesystem/src/main/java/com/mms/medmanagesystem/controller/PacienteController.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -50,8 +51,11 @@ public class PacienteController {
     return modelAndView;
   }
   
-
-/* 
+ /*  @GetMapping("/pessoas/{id}")
+  public Paciente getPessoaBycc(@PathVariable(value="id") int id) throws ResourceNotFoundException {
+      return pacienteService.getPacienteById(id);
+  } */
+  /* 
   @RequestMapping(value="name", method= {RequestMethod.GET})
   public String name(@RequestParam String name, Model model){
       String name =   
