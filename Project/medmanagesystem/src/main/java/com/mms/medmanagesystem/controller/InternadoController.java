8a3e74a1e25919bfebce90a12f1c302aec25a5ef
artur-romao <arturomao@gmail.com>
2022-01-17 22:29:39 +0000

    developing internados features

--- Project/medmanagesystem/src/main/java/com/mms/medmanagesystem/controller/InternadoController.java
+++ Project/medmanagesystem/src/main/java/com/mms/medmanagesystem/controller/InternadoController.java
@@ -1,5 +1,12 @@
 package com.mms.medmanagesystem.controller;
 
+import javax.servlet.http.HttpSession;
+
+import com.mms.medmanagesystem.service.InternamentoService;
+import com.mms.medmanagesystem.service.ProfissionalService;
+
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +14,15 @@ import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class InternadoController {
+  
+  @Autowired
+  ProfissionalService profissionalService;
+  
+  @Autowired 
+  InternamentoService internadoService;
+
+  @Autowired
+  ObjectFactory<HttpSession> httpSessionFactory;
 
   @GetMapping("/internados")
   public ModelAndView internado(Model model) {
