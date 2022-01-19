197b29a05f8f497276cfe09fdbed3eb65da7a5b1
artur-romao <arturomao@gmail.com>
2022-01-19 22:38:37 +0000

    trying new stuff

---
+++ Project/medmanagesystem/src/main/resources/static/js/consultas.js
@@ -0,0 +1,30 @@
+[
+    {
+      title          : 'Cirurgia ao pâncreas Afonso Costa',
+      start          : new Date(y, m, 7, 9, 30, 17, 30),
+      allDay         : false,
+      backgroundColor: '#f56954', //red
+      borderColor    : '#f56954', //red
+    },
+    {
+      title          : 'MedLabs - Palestras sobre novos fármacos',
+      start          : new Date(y, m, d - 2),
+      end            : new Date(y, m, d),
+      backgroundColor: '#f39c12', //yellow
+      borderColor    : '#f39c12' //yellow
+    },
+    {
+      title          : 'Teleconsulta Joana Gomes',
+      start          : new Date(y, m, d, 10, 30),
+      allDay         : false,
+      backgroundColor: '#0073b7', //Blue
+      borderColor    : '#0073b7' //Blue
+    },
+    {
+      title          : 'Concerto Joana Gomes',
+      start          : new Date(2022, 1-1, 19, 12, 30),
+      allDay         : false,
+      backgroundColor: '#0073b7', //Blue
+      borderColor    : '#0073b7' //Blue
+    }
+]
